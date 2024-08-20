d25d3eb0c24206269e1ff727ff08874ce1b6814f
isabeleoli <130056752+isabeleoli@users.noreply.github.com>
2024-08-20 07:46:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -32,41 +32,41 @@ const perguntas = [
         ]
     },
     {
-        enunciado: " ",
+        enunciado: "Moda 2024  ",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Ela esta transformando visões.",
+                afirmacao: "A moda transaforma, muda a maneira das pessoas verem a vida."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "A cada ano que passa, as tendencias de moda vão se inovando.",
+                afirmacao: "Isso é excelente."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "As tendencias deste ano",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Com a estética ultra feminina, os laços foram uma das tendencias de moda de 2024",
+                afirmacao: "Eles mudam o visual."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Eles dominaram as passarelas, sendo incluídos em desfiles, de diversas formas.",
+                afirmacao: "Uma ideia sensacional"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Os novos tons",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Este ano os tons de marrom são uma aposta certa para o Outono/Inverno.",
+                afirmacao: "Deixando as passarelas com um tom a mais de elegancia."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "tendencia 2025.",
+                afirmacao: "ja para o próximo ano, a proposta para o ano será o amarelo, que esteve muito presente nas passarelas, e que pretendem tomar lugar no dia a dia da sociedade. "
             }
         ]
     },
